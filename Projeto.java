285bbc1e7ff2d32a16469821d08aee9c8e2f9685
Kelly Kimura <akellykimura007@gmail.com>
2024-08-26 18:22:38 -0300

    Projeto classe

---
+++ Projeto.java
@@ -0,0 +1,28 @@
+public class Projeto {
+    
+    protected int contTarefas;
+    protected int max;
+    protected String nomeProjeto;
+    protected Tarefa vetorTarefas[];
+
+    //metodo construtor
+    public Projeto(String nomeProjeto)
+    {
+        contTarefas = 0; //esse é só pra esse objeto, ou para toda a classe?
+        max = 50;
+        this.nomeProjeto = nomeProjeto;
+        vetorTarefas = new Tarefa[max];
+    }
+
+    //getters e setters
+    public String getNomeProjeto ()
+    {
+        return nomeProjeto;
+    }
+
+    public void setNomeProjeto (String nomeProjeto)
+    {
+        this.nomeProjeto = nomeProjeto;
+    }
+
+}
