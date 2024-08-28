ef839e5d7010e5153563a31d11a59ab192e0d3d5
Kelly Kimura <akellykimura007@gmail.com>
2024-08-28 15:31:28 -0300

    refactor: edit names

--- SistemaGerenciamentoTarefas/src/Classes/Projeto.java
+++ SistemaGerenciamentoTarefas/src/Classes/Projeto.java
@@ -1,28 +1,28 @@
 package SistemaGerenciamentoTarefas.src.Classes;
 public class Projeto {
     
-    protected int contTarefas;
+    protected int countTask;
     protected int max;
-    protected String nomeProjeto;
-    protected Tarefa vetorTarefas[];
+    protected String name;
+    protected Tarefa arrayTask[];
 
     //metodo construtor
-    public Projeto(String nomeProjeto)
+    public Projeto(String name)
     {
-        contTarefas = 0; //esse é só pra esse objeto, ou para toda a classe?
+        countTask = 0; //esse é só pra esse objeto, ou para toda a classe?
         max = 50;
-        this.nomeProjeto = nomeProjeto;
-        vetorTarefas = new Tarefa[max];
+        this.name = name;
+        arrayTask = new Tarefa[max];
     }
 
-    public String getNomeProjeto ()
+    public String getName ()
     {
-        return nomeProjeto;
+        return name;
     }
 
-    public void setNomeProjeto (String nomeProjeto)
+    public void setName (String name)
     {
-        this.nomeProjeto = nomeProjeto;
+        this.name = name;
     }
 
 }
