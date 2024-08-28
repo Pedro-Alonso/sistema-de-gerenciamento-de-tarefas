69c1adfc76bcfdae6e5fe2d734e1a4664088beaf
Kelly Kimura <akellykimura007@gmail.com>
2024-08-28 18:05:58 -0300

    feat: added new methods

--- SistemaGerenciamentoTarefas/src/Classes/Projeto.java
+++ SistemaGerenciamentoTarefas/src/Classes/Projeto.java
@@ -1,4 +1,4 @@
-package SistemaGerenciamentoTarefas.src.Classes;
+package SistemaGerenciamentoTasks.src.Classes;
 
 import java.time.LocalDate;
 
@@ -9,16 +9,70 @@ public class Projeto {
     protected LocalDate limitDate;
     protected int max;
     protected String name;
-    protected Tarefa arrayTask[];
+    protected Task taskArray[];
 
-    //metodo construtor
-    public Projeto(String name)
+    //constructor method
+    public Projeto(LocalDate beginDate, LocalDate limitDate, String name)
     {
+        this.beginDate = beginDate;
+        this.limitDate = limitDate;
         taskCount = 0; //esse é só pra esse objeto, ou para toda a classe?
         max = 50;
         this.name = name;
-        arrayTask = new Tarefa[max];
+        taskArray = new Task[max];
     }
+
+    //method to search tasks of the project
+    public int searchTask (String id)
+    {
+        for (int i = 0; i < taskCount; i++)
+        {
+            if (taskArray[i].getId().equals(id))
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    //methods to add and remove tasks from the array
+    public void addTask (Task t)
+    {
+        if(taskCount < max)
+        {
+            taskArray[taskCount] = t;
+            taskCount++;
+        }
+    }
+
+    public void removeTask (String id)
+    {
+        int i = searchTask(id);
+
+        if(i != -1)
+        {
+            if (i < taskCount)
+            {
+                int j = 0;
+                for (j = i; j < taskCount; j++)
+                {
+                    taskArray[j] = taskArray[j+1];
+
+                }
+
+                taskArray[taskCount - 1] = null;
+                taskCount--;
+            }
+            else //if i == taskCount means that you found the task in the last array's position
+            {
+                taskArray[i] = null;
+                taskCount--;
+            }
+        }
+    }
+
+
 
     //getters e setters
     public LocalDate getBeginDate()
