20d1eeb1c692ea7bcecc58223bddab35b1ef657d
Pedro Alonso <pedro.alonsodeoliveira@hotmail.com>
2024-09-05 15:57:18 -0300

    docs: improved javadoc

--- SistemaGerenciamentoTarefas/src/Classes/Task.java
+++ SistemaGerenciamentoTarefas/src/Classes/Task.java
@@ -25,11 +25,11 @@ public class Task {
 
   /**
    * Constructor for the Task class
-   * @param userId The id of the user that owns the Task
-   * @param name The name of the Task
-   * @param description The description of the Task
-   * @param deadline The deadline of the Task
-   * @param priority The priority of the Task
+   * @param userId The id of the user that owns the Task -> {@link UUID}
+   * @param name The name of the Task -> {@link String}
+   * @param description The description of the Task -> {@link String}
+   * @param deadline The deadline of the Task -> {@link LocalDate}
+   * @param priority The priority of the Task -> {@code int}
    */
   public Task(
     UUID userId,
@@ -50,24 +50,21 @@ public class Task {
   }
 
   /**
-   * Getter for the id of the Task
-   * @return UUID
+   * {@return the id of the Task as a {@link UUID}}
    */
   public UUID getid() {
     return id;
   }
 
   /**
-   * Getter for the userId of the Task (owner)
-   * @return UUID
+   * {@return the id of the user that owns the Task as a {@link UUID}}
    */
   public UUID getUserId() {
     return userId;
   }
 
   /**
-   * Getter for the name of the Task
-   * @return String
+   * {@return the name of the Task as a {@link String}}
    */
   public String getName() {
     return name;
@@ -75,15 +72,14 @@ public class Task {
 
   /**
    * Setter for the name of the Task
-   * @param name The new name for the Task
+   * @param name The new name for the Task -> {@link String}
    */
   public void setName(String name) {
     this.name = name;
   }
 
   /**
-   * Getter for the description of the Task
-   * @return String
+   * {@return the description of the Task as a {@link String}}
    */
   public String getDescription() {
     return description;
@@ -91,15 +87,14 @@ public class Task {
 
   /**
    * Setter for the description of the Task
-   * @param description The new description for the Task
+   * @param description The new description for the Task -> {@link String}
    */
   public void setDescription(String description) {
     this.description = description;
   }
 
   /**
-   * Getter for the deadline of the Task
-   * @return LocalDate
+   * {@return the deadline of the Task as a {@link LocalDate}}
    */
   public LocalDate getDeadline() {
     return deadline;
@@ -107,15 +102,14 @@ public class Task {
 
   /**
    * Setter for the deadline of the Task
-   * @param deadline The new deadline for the Task
+   * @param deadline The new deadline for the Task -> {@link LocalDate}
    */
   public void setDeadline(LocalDate deadline) {
     this.deadline = deadline;
   }
 
   /**
-   * Getter for the priority of the Task
-   * @return int
+   * {@return the priority of the Task as a {@code int}}
    */
   public int getPriority() {
     return priority;
@@ -123,15 +117,14 @@ public class Task {
 
   /**
    * Setter for the priority of the Task
-   * @param priority The new priority for the Task
+   * @param priority The new priority for the Task -> {@code int}
    */
   public void setPriority(int priority) {
     this.priority = priority;
   }
 
   /**
-   * Getter for the status of the Task
-   * @return Status
+   * {@return the status of the Task as a {@link Status}}
    */
   public Status getStatus() {
     return status;
@@ -139,23 +132,21 @@ public class Task {
 
   /**
    * Setter for the status of the Task
-   * @param status The new status for the Task
+   * @param status The new status for the Task -> {@link Status}
    */
   public void setStatus(Status status) {
     this.status = status;
   }
 
   /**
-   * Getter for the tags of the Task
-   * @return ArrayList<Tag>
+   * {@return the tags of the Task as a {@link ArrayList} of {@link Tag}}
    */
   public ArrayList<Tag> getTags() {
     return tags;
   }
 
   /**
-   * Getter for the subTasks of the Task
-   * @return ArrayList<SubTask>
+   * {@return the subTasks of the Task as a {@link ArrayList} of {@link SubTask}}
    */
   public ArrayList<SubTask> getSubTasks() {
     return subTasks;
@@ -163,8 +154,7 @@ public class Task {
 
   /**
    * Method to add a Tag to the current Task object. If the Tag already exists on the Task tags, it will not be added
-   * @category Method
-   * @return void
+   * @param tag The Tag object to be added to the Task tags -> {@link Tag}
    */
   public void addTag(Tag tag) {
     Tag t = getTagById(tag.getId());
@@ -174,8 +164,8 @@ public class Task {
 
   /**
    * Method to get the Tag object on this Task tags, if exists
-   * @param id
-   * @return Tag -> Tag if exists, null if not
+   * @param id The id of the Tag to be searched -> {@link UUID}
+   * @return {@link Tag} | {@code null} if not exists
    */
   public Tag getTagById(UUID id) {
     for (Tag tag : tags) {
@@ -186,8 +176,8 @@ public class Task {
 
   /**
    * Method to get the index of a specific Tag on this Task tags
-   * @param id
-   * @return int -> index if exists, -1 if not
+   * @param id The id of the Tag to be searched -> {@link UUID}
+   * @return {@code int} | {@code -1} if not exists
    */
   private int getTagIndex(UUID id) {
     int index = -1;
@@ -202,8 +192,8 @@ public class Task {
 
   /**
    * Method to remove a Tag from this Task tags of a given ig
-   * @param id
-   * @return boolean -> true if removed, false if not exists
+   * @param id The id of the Tag to be removed -> {@link UUID}
+   * @return {@code boolean} | {@code true} if removed, {@code false} if not exists
    */
   public boolean removeTag(UUID id) {
     int tagIndex = getTagIndex(id);
@@ -214,8 +204,7 @@ public class Task {
 
   /**
    * Method to add a SubTask to the current Task object
-   * @category Method
-   * @return void
+   * @param subTask The SubTask object to be added to the Task subTasks -> {@link SubTask}
    */
   public void addSubTask(SubTask subTask) {
     SubTask st = getSubTaskById(subTask.getid());
@@ -225,8 +214,8 @@ public class Task {
 
   /**
    * Method to get the SubTask object on this Task subTasks, if exists
-   * @param id
-   * @return SubTask -> SubTask if exists, null if not
+   * @param id The id of the SubTask to be searched -> {@link UUID}
+   * @return {@link SubTask} | {@code null} if not exists
    */
   public SubTask getSubTaskById(UUID id) {
     for (SubTask subTask : subTasks) {
@@ -237,8 +226,8 @@ public class Task {
 
   /**
    * Method to get the index of a specific SubTask on this Task subTasks
-   * @param id
-   * @return int -> index if exists, -1 if not
+   * @param id The id of the SubTask to be searched -> {@link UUID}
+   * @return {@code int} | {@code -1} if not exists
    */
   private int getSubTaskIndex(UUID id) {
     int index = -1;
@@ -253,8 +242,8 @@ public class Task {
 
   /**
    * Method to remove a SubTask from this Task subTasks of a given ig
-   * @param id
-   * @return boolean -> true if removed, false if not exists
+   * @param id The id of the SubTask to be removed -> {@link UUID}
+   * @return {@code boolean} | {@code true} if removed, {@code false} if not exists
    */
   public boolean removeSubTask(UUID id) {
     int subTaskIndex = getSubTaskIndex(id);
@@ -265,11 +254,10 @@ public class Task {
 
   /**
    * Method to get all elements of a given ArrayList<T> and return a String with their names, using the equivalent getName() method of the T class
-   * @category Method
-   * @param array ArrayList if T class
-   * @param getNameFunc Equivalent function of getName() on T class
-   * @return void
+   * @param array The ArrayList<> of given class T to be used -> {@link ArrayList}
+   * @param getNameFunc Equivalent function of getName() on T class -> {@link Function}
    *
+   * @return {@link String}
    * Example: getListString(tasks, Task::getName);
    */
   private <T> String getListString(
@@ -288,8 +276,7 @@ public class Task {
 
   /**
    * Method that returns a String with all the information about the current Task object, except for the id
-   * @category Method
-   * @return String
+   * @return {@link String}
    */
   public String printTask() {
     String tagNames = getListString(tags, Tag::getDescription);
@@ -328,8 +315,6 @@ public class Task {
 
   /**
    * Method to increment (by one) the priority of the current Task object
-   * @category Method
-   * @return void
    */
   public void incrementPriority() {
     this.priority += 1;
@@ -337,8 +322,6 @@ public class Task {
 
   /**
    * Method to decrement (by one) the priority of the current Task object
-   * @category Method
-   * @return void
    */
   public void decrementPriority() {
     this.priority -= 1;
