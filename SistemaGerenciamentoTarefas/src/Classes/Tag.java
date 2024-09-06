219ea6b3cc95ff08b3469c2a9bbe8caea2f9dcda
Pedro Alonso <pedro.alonsodeoliveira@hotmail.com>
2024-09-06 16:47:50 -0300

    fix: Tag package and getUserId fix

--- SistemaGerenciamentoTarefas/src/Classes/Tag.java
+++ SistemaGerenciamentoTarefas/src/Classes/Tag.java
@@ -1,9 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-package Tag;
+package SistemaGerenciamentoTarefas.src.Classes;
 
 import java.util.UUID;
 /**
@@ -22,7 +17,7 @@ public class Tag {
         this.id = UUID.randomUUID();
     }
     
-    public UUID getuserID(){
+    public UUID getUserID(){
         return userId;
     }
 
