a8aac09a259295cd19b965765c5c2e3a3df37e65
Pedro Alonso <pedro.alonsodeoliveira@hotmail.com>
2024-11-12 15:46:52 -0300

    docs: documenting Logger and LoggerRecord

--- SistemaGerenciamentoTarefas/src/Classes/Logger.java
+++ SistemaGerenciamentoTarefas/src/Classes/Logger.java
@@ -2,14 +2,24 @@ package SistemaGerenciamentoTarefas.src.Classes;
 
 import java.util.ArrayList;
 
+/**
+ * Singleton class representing a Logger in the task management system.
+ */
 public class Logger {
     private static Logger singleInstance;
     private ArrayList<LoggerRecord> logs;
 
+    /**
+     * Private constructor to prevent instantiation.
+     */
     private Logger() {
         logs = new ArrayList<>();
     }
 
+    /**
+     * Method to get the single instance of the Logger class.
+     * @return {@link Logger} The single instance of the Logger class.
+     */
     public static synchronized Logger getInstance() {
         if (singleInstance == null) {
             singleInstance = new Logger();
@@ -17,20 +27,33 @@ public class Logger {
         return singleInstance;
     }
 
-    public void publish (LoggerRecord log) {
+    /**
+     * Method to publish a log record.
+     * @param log The log record to be published -> {@link LoggerRecord}
+     */
+    public void publish(LoggerRecord log) {
         logs.add(log);
     }
 
-    public void flush () {
+    /**
+     * Method to clear all log records.
+     */
+    public void flush() {
         logs.clear();
     }
 
+    /**
+     * {@return the list of log records as an {@link ArrayList} of {@link LoggerRecord}}
+     */
     public ArrayList<LoggerRecord> getLogs() {
         return logs;
     }
 
+    /**
+     * Setter for the list of log records.
+     * @param logs The new list of log records -> {@link ArrayList} of {@link LoggerRecord}
+     */
     public void setLogs(ArrayList<LoggerRecord> logs) {
         this.logs = logs;
     }
-
 }
--- SistemaGerenciamentoTarefas/src/Classes/LoggerRecord.java
+++ SistemaGerenciamentoTarefas/src/Classes/LoggerRecord.java
@@ -2,13 +2,21 @@ package SistemaGerenciamentoTarefas.src.Classes;
 
 import java.time.LocalDateTime;
 
+/**
+ * Class representing a log record in the task management system.
+ */
 public class LoggerRecord {
     private Object editor;    
     private LocalDateTime createdAt;
-    
     private String subject;
     private String message;
 
+    /**
+     * Constructor for the LoggerRecord class with an editor.
+     * @param editor The editor of the log record -> {@link UserTask}
+     * @param subject The subject of the log record -> {@link String}
+     * @param message The message of the log record -> {@link String}
+     */
     public LoggerRecord(UserTask editor, String subject, String message) {
         setEditor(editor);
         setCreatedAt();
@@ -16,6 +24,11 @@ public class LoggerRecord {
         this.message = message;
     }
 
+    /**
+     * Constructor for the LoggerRecord class without an editor.
+     * @param subject The subject of the log record -> {@link String}
+     * @param message The message of the log record -> {@link String}
+     */
     public LoggerRecord(String subject, String message) {
         setEditor("SISTEMA");
         setCreatedAt();
@@ -23,10 +36,18 @@ public class LoggerRecord {
         this.message = message;
     }
 
+    /**
+     * {@return the editor of the log record as an {@link Object}}
+     */
     public Object getEditor() {
         return editor;
     }
 
+    /**
+     * Setter for the editor of the log record.
+     * @param editor The new editor for the log record -> {@link Object}
+     * @throws IllegalArgumentException if the editor is not of type UserTask or String
+     */
     private void setEditor(Object editor) {
         if (editor instanceof UserTask || editor instanceof String) {
             this.editor = editor;
@@ -35,30 +56,47 @@ public class LoggerRecord {
         }
     }
 
+    /**
+     * {@return the creation date of the log record as a {@link LocalDateTime}}
+     */
     public LocalDateTime getCreatedAt() {
         return createdAt;
     }
 
+    /**
+     * Setter for the creation date of the log record.
+     */
     private void setCreatedAt() {
         createdAt = LocalDateTime.now();
     }
 
+    /**
+     * {@return the subject of the log record as a {@link String}}
+     */
     public String getSubject() {
         return subject;
     }
 
+    /**
+     * Setter for the subject of the log record.
+     * @param subject The new subject for the log record -> {@link String}
+     */
     public void setSubject(String subject) {
         this.subject = subject;
     }
 
+    /**
+     * {@return the message of the log record as a {@link String}}
+     */
     public String getMessage() {
         return message;
     }
 
+    /**
+     * Setter for the message of the log record.
+     * @param message The new message for the log record -> {@link String}
+     */
     public void setMessage(String message) {
         this.message = message;
     }
-
-
-
 }
